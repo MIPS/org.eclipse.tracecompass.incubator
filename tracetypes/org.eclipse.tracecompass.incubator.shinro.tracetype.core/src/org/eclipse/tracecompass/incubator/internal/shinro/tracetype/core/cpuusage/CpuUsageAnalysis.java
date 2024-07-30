84d5501f0282151fe73b8e9f030734c9bfa0c53d
Greg Savin <gsavin@mips.com>
2024-07-30 09:43:09 -0700

    This gets some fake data rendered.

--- tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/internal/shinro/tracetype/core/cpuusage/CpuUsageAnalysis.java
+++ tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/internal/shinro/tracetype/core/cpuusage/CpuUsageAnalysis.java
@@ -1,8 +1,14 @@
 package org.eclipse.tracecompass.incubator.internal.shinro.tracetype.core.cpuusage;
 
+import java.util.Objects;
+
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.tracecompass.statesystem.core.ITmfStateSystemBuilder;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.statesystem.AbstractTmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.ITmfStateProvider;
 import org.eclipse.tracecompass.tmf.core.statesystem.TmfStateSystemAnalysisModule;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 
 public class CpuUsageAnalysis extends TmfStateSystemAnalysisModule {
 
@@ -15,8 +21,42 @@ public class CpuUsageAnalysis extends TmfStateSystemAnalysisModule {
 
     @Override
     protected @NonNull ITmfStateProvider createStateProvider() {
-        ITmfStateProvider bogus = (ITmfStateProvider)new Object();
-        return bogus;
+        return new CpuUsageStateProvider(Objects.requireNonNull(getTrace()), CpuUsageAnalysis.ID);
     }
 
+}
+
+
+class CpuUsageStateProvider extends AbstractTmfStateProvider {
+
+    String moduleId;
+
+    long tempEventsSeen = 0;
+
+    public CpuUsageStateProvider(@NonNull ITmfTrace trace, @NonNull String moduleId) {
+        super(trace, moduleId);
+        this.moduleId = moduleId;
+    }
+
+    @Override
+    public int getVersion() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public @NonNull ITmfStateProvider getNewInstance() {
+        return new CpuUsageStateProvider(getTrace(), moduleId);
+    }
+
+    @Override
+    protected void eventHandle(@NonNull ITmfEvent event) {
+        ITmfStateSystemBuilder ss = getStateSystemBuilder();
+        if (ss == null) {
+            return;
+        }
+        tempEventsSeen++;
+        int fooQuark = ss.getQuarkAbsoluteAndAdd("foo", "bar");
+        ss.modifyAttribute(event.getTimestamp().toNanos(), Long.valueOf(tempEventsSeen), fooQuark);
+    }
 }
--- tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/internal/shinro/tracetype/core/cpuusage/CpuUsageDataProvider.java
+++ tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/internal/shinro/tracetype/core/cpuusage/CpuUsageDataProvider.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.annotation.NonNull;
@@ -13,6 +14,7 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tracecompass.internal.tmf.core.model.filters.FetchParametersUtils;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystem;
 import org.eclipse.tracecompass.statesystem.core.exceptions.StateSystemDisposedException;
+import org.eclipse.tracecompass.tmf.core.model.YModel;
 import org.eclipse.tracecompass.tmf.core.model.filters.TimeQueryFilter;
 import org.eclipse.tracecompass.tmf.core.model.tree.TmfTreeModel;
 import org.eclipse.tracecompass.tmf.core.model.xy.AbstractTreeCommonXDataProvider;
@@ -24,6 +26,9 @@ import com.google.common.collect.ImmutableList;
 
 public class CpuUsageDataProvider extends AbstractTreeCommonXDataProvider<@NonNull CpuUsageAnalysis, @NonNull CpuUsageEntryModel> {
     public static final String ID = "org.eclipse.tracecompass.incubator.internal.shinro.tracetype.core.cpuusage.CpuUsageDataProvider"; //$NON-NLS-1$
+
+    private static final AtomicLong TRACE_IDS = new AtomicLong();
+    private final long fBogusId = TRACE_IDS.getAndIncrement();
 
     public static @Nullable CpuUsageDataProvider create(ITmfTrace trace) {
         CpuUsageAnalysis module = TmfTraceUtils.getAnalysisModuleOfClass(trace, CpuUsageAnalysis.class, CpuUsageAnalysis.ID);
@@ -48,9 +53,15 @@ public class CpuUsageDataProvider extends AbstractTreeCommonXDataProvider<@NonNu
 
     @Override
     protected @Nullable Collection<@NonNull IYModel> getYSeriesModels(@NonNull ITmfStateSystem ss, @NonNull Map<@NonNull String, @NonNull Object> fetchParameters, @Nullable IProgressMonitor monitor) throws StateSystemDisposedException {
-        // TODO Auto-generated method stub
-        return null;
+        return makeFakeData();
     }
+
+    private Collection<@NonNull IYModel> makeFakeData() {
+        ArrayList<IYModel> list = new ArrayList<>();
+        list.add(new YModel(this.fBogusId, new double[] {1.2, 2.3, 3.4}));
+        return list;
+    }
+
 
     @Override
     protected @NonNull String getTitle() {
@@ -72,6 +83,11 @@ public class CpuUsageDataProvider extends AbstractTreeCommonXDataProvider<@NonNu
         //long end = filter.getEnd();
 
         List<CpuUsageEntryModel> entryList = new ArrayList<>();
+        var builder = new ImmutableList.Builder<String>();
+        builder.add("tree item");
+        var entry = new CpuUsageEntryModel(0, -1, builder.build());
+        entryList.add(entry);
+
         return new TmfTreeModel<>(ImmutableList.of("Column 1", "Column 2", "Column 3"), entryList);
     }
 
