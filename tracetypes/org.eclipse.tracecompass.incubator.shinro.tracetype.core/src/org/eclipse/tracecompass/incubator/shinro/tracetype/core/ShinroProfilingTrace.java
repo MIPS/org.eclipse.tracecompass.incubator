19e834d2474ea26fe6abc3dca3b25e25e2a71782
Greg Savin <gsavin@mips.com>
2024-09-23 16:32:54 -0700

    Switching back to jhdf5, to work around apparent bug in libhdf5. Still need to incorporate disassembly text.

--- tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/shinro/tracetype/core/ShinroProfilingTrace.java
+++ tracetypes/org.eclipse.tracecompass.incubator.shinro.tracetype.core/src/org/eclipse/tracecompass/incubator/shinro/tracetype/core/ShinroProfilingTrace.java
@@ -1,9 +1,11 @@
 package org.eclipse.tracecompass.incubator.shinro.tracetype.core;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.math.BigInteger;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
@@ -23,103 +25,99 @@ import org.eclipse.tracecompass.tmf.core.trace.TraceValidationStatus;
 import org.eclipse.tracecompass.tmf.core.trace.location.ITmfLocation;
 import org.eclipse.tracecompass.tmf.core.trace.location.TmfLongLocation;
 
-import hdf.hdf5lib.H5;
-import hdf.hdf5lib.HDF5Constants;
-import hdf.hdf5lib.callbacks.H5D_iterate_cb;
-import hdf.hdf5lib.callbacks.H5D_iterate_t;
-import hdf.hdf5lib.exceptions.HDF5Exception;
-import hdf.hdf5lib.exceptions.HDF5FileInterfaceException;
+import io.jhdf.HdfFile;
+import io.jhdf.api.Dataset;
+import io.jhdf.api.Group;
+import io.jhdf.api.Node;
+import io.jhdf.api.NodeType;
+import io.jhdf.exceptions.HdfException;
+import io.jhdf.object.datatype.CompoundDataType;
+import io.jhdf.object.datatype.CompoundDataType.CompoundDataMember;
+import io.jhdf.object.datatype.DataType;
 
 public class ShinroProfilingTrace extends TmfTrace {
 
+    Map<String, Object> f_instProfData = null;
+    long f_instProfDataNumElements;
+    long f_rank;
+    Map<String, DatasetMetadata> f_instProfMetadata = new HashMap<>();
+
+    class DatasetMetadata {
+        DatasetMetadata(CompoundDataMember member) {
+            this.member = member;
+            DataType mdt = member.getDataType();
+            cls = mdt.getJavaType();
+        }
+        CompoundDataMember member;
+        Class<?> cls;
+    }
+
     private static final int CONFIDENCE = 100;
 
-    long f_file_id = HDF5Constants.H5I_INVALID_HID;
-    long f_dataset_id = HDF5Constants.H5I_INVALID_HID;
-    long f_dataspace_id = HDF5Constants.H5I_INVALID_HID;
-    long f_num_elements = 0;
-    long f_rank = 0;
-    Hdf5libProfilingDataSlicedAccessor f_accessor = null;
-
-
-    static private boolean isShinroProfilingHdf5File(String path) {
+    static private boolean isShinroProfilingHdf5File(String strPath) {
         // a file is deemed to be a Shinro profiling HDF5 file if it can be opened
         // successfully with the HDF5 library, and if it has a group /inst_prof_data.
-        try {
-            boolean valid = false;
-            long file_id = H5.H5Fopen(path, HDF5Constants.H5F_ACC_RDONLY, HDF5Constants.H5P_DEFAULT);
-            boolean opened = file_id != HDF5Constants.H5I_INVALID_HID;
-            if (opened) {
-                try {
-                    long group_id = H5.H5Gopen(file_id, "/inst_prof_data", HDF5Constants.H5P_DEFAULT);
-                    valid = group_id >= 0;
-                    if (valid) {
-                        H5.H5Gclose(group_id);
-                    }
-                } catch (HDF5Exception e) {
-                    // This means that the HDF5 file doesn't have the expected group, so it's
-                    // OK to swallow this exception, and assume that this is not a Shinro profiling HDF5 file
-                } finally {
-                    H5.H5Fclose(file_id);
-                }
-            }
-            return valid;
-        } catch (HDF5FileInterfaceException e) {
-            return false;
+        boolean valid = false;
+        Path path = Paths.get(strPath);
+        try (HdfFile hdfFile = new HdfFile(path)) {
+            // consider the answer being yes iff the file opens successfully as an HDF5 file,
+            // and if the group /inst_prof_data is present
+            Node nodeProfData = hdfFile.getByPath("/inst_prof_data");
+            valid = (nodeProfData != null && nodeProfData.getType() == NodeType.GROUP);
+        } catch (HdfException e) {
+
         }
+        return valid;
     }
 
     @Override
-    public void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type, String name, String traceTypeId) throws TmfTraceException {
-        f_file_id = H5.H5Fopen(path, HDF5Constants.H5F_ACC_RDONLY, HDF5Constants.H5P_DEFAULT);
-        if (f_file_id != HDF5Constants.H5I_INVALID_HID) {
-            // We can't statically know the *name* of the expected dataset, but we do statically know the position (0).
-            // The HDF5Dopen API wants a name, so we have an extra step of looking up the name from the known index.
-            String [] nameOut = new String[1];
-            int [] typeOut = new int[1];
-            int indexOfExpectedDataset = 0;
-            H5.H5Gget_obj_info_idx(f_file_id, "/inst_prof_data", indexOfExpectedDataset, nameOut, typeOut);
+    public void initTrace(IResource resource, String strPath, Class<? extends ITmfEvent> type, String name, String traceTypeId) throws TmfTraceException {
+        Path path = Paths.get(strPath);
+        try (HdfFile file = new HdfFile(path)) {
+            Node nodeProfData = file.getByPath("/inst_prof_data");
 
-            // Now the we have the name of the dataset we're interested in, we can open it and query for
-            // additional information we need
-            f_dataset_id = H5.H5Dopen(f_file_id, "/inst_prof_data/" + nameOut[0], HDF5Constants.H5P_DEFAULT);
-            f_dataspace_id = H5.H5Dget_space(f_dataset_id);
-            f_num_elements = H5.H5Sget_simple_extent_npoints(f_dataspace_id);
+            if (nodeProfData != null && nodeProfData.getType() == NodeType.GROUP) {
+                Group groupProfData = (Group)nodeProfData;
+                // find first dataset (we don't statically know the name at this point)
+                Dataset firstDataset = null;
+                var children = groupProfData.getChildren();
+                var childIterator = children.values().iterator();
+                while (childIterator.hasNext()) {
+                    var child = childIterator.next();
+                    if (child.getType() == NodeType.DATASET) {
+                        firstDataset = (Dataset)child;
+                        break;
+                    }
+                }
+                if (firstDataset != null) {
+                    f_instProfDataNumElements = firstDataset.getDimensions()[0];
+                    DataType dt = firstDataset.getDataType();
+                    CompoundDataType cdt = (CompoundDataType)dt;
+                    List<CompoundDataMember> members = cdt.getMembers();
+                    for (int idx = 0; idx < members.size(); idx++) {
+                        CompoundDataMember member = members.get(idx);
+                        f_instProfMetadata.put(member.getName(), new DatasetMetadata(member));
+                    }
+                    Object obj = firstDataset.getData();
+                    if (obj instanceof Map<?, ?>) {
+                        f_instProfData = (Map<String, Object>)firstDataset.getData();
+                    }
 
-            loadDasmData(f_file_id);
+                }
+                // TODO - CONSIDER MULTI-CORE!  Currently getting single core working, but there will probably be one
+                // dataset per core for multi-core.  So this metehod, and class member data structures will probably need
+                // to be adjusted to be per-core.
+            }
+        } catch (HdfException e) {
 
-            // TODO - CONSIDER MULTI-CORE!  Currently getting single core working, but there will probably be one
-            // dataset per core for multi-core.  So this metehod, and class member data structures will probably need
-            // to be adjusted to be per-core.
         }
-        super.initTrace(resource, path, type, name, traceTypeId);
+        super.initTrace(resource, strPath, type, name, traceTypeId);
     }
 
-    static class IterateCallback implements H5D_iterate_cb {
-        /**
-         * @param elem
-         * @param elem_type
-         * @param ndim
-         * @param point
-         * @param op_data
-         */
-        @Override
-        public int callback (byte[] elem, long elem_type, int ndim, long[] point, H5D_iterate_t op_data) {
-            return 0;
-        }
-    }
 
-    static class IterateData implements H5D_iterate_t {
 
-    }
+/*
     private static void loadDasmData(long file_id) {
-        /*
-        String [] nameOut = new String[1];
-        int [] typeOut = new int[1];
-        int indexOfExpectedDataset = 1;
-        H5.H5Gget_obj_info_idx(file_id, "/", indexOfExpectedDataset, nameOut, typeOut);
-        long dasm_dataset_id = H5.H5Dopen(file_id, "/" + nameOut[0], HDF5Constants.H5P_DEFAULT);
-        */
 
         long dasm_dataset_id = H5.H5Dopen(file_id, "/inst_disasm_data", HDF5Constants.H5P_DEFAULT);
         long dasm_dataspace_id = H5.H5Dget_space(dasm_dataset_id);
@@ -181,25 +179,10 @@ public class ShinroProfilingTrace extends TmfTrace {
             H5.H5Dclose(dasm_dataset_id);
         }
     }
+    */
 
     @Override
     public synchronized void dispose() {
-        if (f_dataspace_id != HDF5Constants.H5I_INVALID_HID) {
-            H5.H5Sclose(f_dataspace_id);
-            f_dataspace_id = HDF5Constants.H5I_INVALID_HID;
-        }
-        if (f_dataset_id != HDF5Constants.H5I_INVALID_HID) {
-            H5.H5Dclose(f_dataset_id);
-            f_dataset_id = HDF5Constants.H5I_INVALID_HID;
-        }
-        if (f_file_id != HDF5Constants.H5I_INVALID_HID) {
-            H5.H5Fclose(f_file_id);
-            f_file_id = HDF5Constants.H5I_INVALID_HID;
-        }
-        // These assignments aren't strictly necessary, but might make it easier when debugging if
-        // things have gone wrong
-        f_num_elements = 0;
-        f_rank = 0;
         super.dispose();
     }
 
@@ -224,7 +207,6 @@ public class ShinroProfilingTrace extends TmfTrace {
 
     @Override
     public ITmfContext seekEvent(ITmfLocation location) {
-        invalidateAccessor();
         if (location == null) {
             f_rank = 0;
         }
@@ -246,19 +228,7 @@ public class ShinroProfilingTrace extends TmfTrace {
         return eventType;
     }
 
-    private Hdf5libProfilingDataSlicedAccessor getAccessor() {
-        if (f_accessor == null) {
-            f_accessor = new Hdf5libProfilingDataSlicedAccessor(f_dataset_id, f_num_elements, f_rank, 1, 1);
-        }
-        return f_accessor;
-    }
 
-    private void invalidateAccessor() {
-        if (f_accessor != null) {
-            f_accessor.dispose();
-            f_accessor = null;
-        }
-    }
 
     @Override
     public ITmfEvent parseEvent(ITmfContext context) {
@@ -266,14 +236,13 @@ public class ShinroProfilingTrace extends TmfTrace {
             System.out.println("Unexpected; figure out an explanation.");
         }
 
-        Hdf5libProfilingDataSlicedAccessor accessor = getAccessor();
-        boolean gotNext = accessor.next();
-        if (!gotNext) {
+        // TODO: return null if f_rank is beyond bounds of what is still unconsumed
+        if (f_rank >= f_instProfDataNumElements) {
             return null;
         }
 
 
-        ITmfEventField content = getFieldContent(accessor);
+        ITmfEventField content = getFieldContent(f_rank);
         ITmfEvent event = new ShinroProfilingEvent(this, f_rank, null, shinroProfilingEventType, content);
 
         // advance rank so that when getCurrentLocation gets called next time, we return
@@ -284,7 +253,11 @@ public class ShinroProfilingTrace extends TmfTrace {
         return event;
     }
 
-    private static ITmfEventField getFieldContent(Hdf5libProfilingDataSlicedAccessor accessor) {
+    /**
+     * @param rank
+     */
+    private ITmfEventField getFieldContent(long rank) {
+        /*
         var map = accessor.members;
         var children = new ArrayList<ITmfEventField>();
         map.forEach((name, info) -> {
@@ -297,121 +270,40 @@ public class ShinroProfilingTrace extends TmfTrace {
             TmfEventField child = new TmfEventField(name, fieldVal, null);
             children.add(child);
         });
+        */
+        // for each field in data map, create a TmfEventField instance with the name of the data
+        // map field and the [f_rank] offset of the array of data values for that field
+        ArrayList<ITmfEventField> children = new ArrayList<>();
+        f_instProfMetadata.forEach((fieldname, metadata) -> {
+            Object field = f_instProfData.get(fieldname);
+            if (field != null) {
+                Object fieldVal = null;
+                if (metadata.cls == BigInteger.class) {
+                    BigInteger [] ary = (BigInteger[])field;
+                    fieldVal = ary[(int)f_rank];
+                } else if (metadata.cls == long.class) {
+                    long [] ary = (long[])field;
+                    fieldVal = ary[(int)f_rank];
+                } else if (metadata.cls == int.class) {
+                    int [] ary = (int[])field;
+                    fieldVal = ary[(int)f_rank];
+                } else if (metadata.cls == double.class) {
+                    double [] ary = (double[])field;
+                    fieldVal = ary[(int)f_rank];
+                } else {
+                    System.out.println("Oops, saw a data type we're not prepared to see");
+                }
+                if (fieldVal != null) {
+                    TmfEventField child = new TmfEventField(fieldname, fieldVal, null);
+                    children.add(child);
+                }
+            }
+        });
+
         TmfEventField rootField = new TmfEventField(":root:", null, children.toArray(new ITmfEventField[0]));
         return rootField;
     }
 
-    static class Hdf5libProfilingDataSlicedAccessor {
-        long [] stride = new long[1];
-        long [] count = new long[1];
-        long [] start = new long[1];
-        long dataset_id;
-        long num_elements;
-        long memspace_id;
-        long filespace_id;
-        long datatype_id;
-        byte [] bytes;
 
-        Map<String, ProfilingDataMemberInfo> members = new HashMap<>();
-
-
-        public Hdf5libProfilingDataSlicedAccessor(long dataset_id, long num_elements, long start, long count, long stride) {
-            this.dataset_id = dataset_id;
-            this.num_elements = num_elements;
-            this.start[0] = start;
-            this.count[0] = count;
-            this.stride[0] = stride;
-            memspace_id = H5.H5Screate_simple(1, this.count, null);
-            filespace_id = H5.H5Dget_space(dataset_id);
-            datatype_id = H5.H5Dget_type(dataset_id);
-
-            // Determine how big of a byte buffer we need, and allocate it
-            int datatype_size = (int)H5.H5Tget_size(datatype_id);
-            bytes = new byte[(int)count * datatype_size];
-
-            // Pre-calculate (one time) the offsets and sizes of members within the raw bytes representation
-            int numMembers = H5.H5Tget_nmembers(datatype_id);
-            for (int memberIdx = 0; memberIdx < numMembers; memberIdx++) {
-                String memberName = H5.H5Tget_member_name(datatype_id, memberIdx);
-                //System.out.println("member name is: " + field);
-                long memberOffset = H5.H5Tget_member_offset(datatype_id, memberIdx);
-                //System.out.println("member offset is: " + memberOffset);
-                long memberType_id = H5.H5Tget_member_type(datatype_id, memberIdx);
-                long memberSize = H5.H5Tget_size(memberType_id);
-                //System.out.println("member size is: " + memberSize);
-
-                members.put(memberName, new ProfilingDataMemberInfo(memberName, memberType_id, memberOffset, memberSize));
-                H5.H5Tclose(memberType_id);
-            }
-
-        }
-
-        public void dispose() {
-            H5.H5Tclose(datatype_id);
-            H5.H5Sclose(filespace_id);
-            H5.H5Sclose(memspace_id);
-        }
-
-        public boolean next() {
-            if (this.start[0] >= num_elements) {
-                return false;
-            }
-            int selectResult = H5.H5Sselect_hyperslab(filespace_id, HDF5Constants.H5S_SELECT_SET, this.start, this.stride, this.count, null);
-            if (selectResult < 0) {
-                return false;
-            }
-
-            int readResult = H5.H5Dread(dataset_id, datatype_id, memspace_id, filespace_id,
-                    HDF5Constants.H5P_DEFAULT, bytes);
-            if (readResult >= 0) {
-                members.forEach((name, info) -> {
-                    if (info.type_class == HDF5Constants.H5T_INTEGER) {
-                        info.longVal = bytesToIntLittleEndian(bytes, (int)info.offset, (int)info.size);
-                        // System.out.println(String.format("%s = 0x%x", name, info.longVal));
-                    } else if (info.type_class == HDF5Constants.H5T_FLOAT) {
-                        info.doubleVal = bytesToDoubleLittleEndian(bytes, (int)info.offset, (int)info.size);
-                        // System.out.println(String.format("%s = %f", name, info.doubleVal));
-                    }
-                });
-
-                // increment position for the next access (in case there is a next access)
-                this.start[0] = this.start[0]+(this.count[0] * this.stride[0]);
-            }
-            return readResult >= 0;
-        }
-    }
-
-    static class ProfilingDataMemberInfo {
-        public String name;
-        public long type_id;
-        public int type_class;
-        public long offset;
-        public long size;
-        public double doubleVal;
-        public long longVal;
-
-        public ProfilingDataMemberInfo(String name, long type_id, long offset, long size) {
-            this.name = name;
-            this.type_id = type_id;
-            this.type_class = H5.H5Tget_class(type_id);
-            this.offset = offset;
-            this.size = size;
-        }
-    }
-
-    static long bytesToIntLittleEndian(byte [] bytes, int offset, int numbytes) {
-        long val = 0;
-
-        for (int i = 0; i < numbytes; i++) {
-            val |= ((bytes[offset+i] & 0xFFL) << (8*i));
-        }
-
-        return val;
-    }
-
-    static double bytesToDoubleLittleEndian(byte [] bytes, int offset, int numbytes) {
-        double d = ByteBuffer.wrap(bytes, offset, numbytes).order(ByteOrder.LITTLE_ENDIAN).getDouble();
-        return d;
-    }
 
 }
